357bfc2a003b735e95b4690673e687a5867501fb
Kaos <dayal700007@gmail.com>
2025-01-27 22:42:39 +0530

    Auto-commit: Changes saved on $(date)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from dotenv import load_dotenv
