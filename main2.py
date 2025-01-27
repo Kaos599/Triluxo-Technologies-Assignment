061863385f00b289794bdadd419caa91fdca3309
Kaos <dayal700007@gmail.com>
2025-01-27 22:59:05 +0530

    Auto-commit: Changes saved on $(date)

--- main2.py
+++ main2.py
@@ -14,39 +14,39 @@ load_dotenv()
 app = Flask(__name__)
 api = Api(app)
 
-# Set the GOOGLE_API_KEY environment variable
+
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 if not os.environ["GOOGLE_API_KEY"]:
     raise ValueError("GOOGLE_API_KEY not found in environment variables.")
 
-# URL of the website to scrape
+
 URL = "https://brainlox.com/courses/category/technical"
 
 
 class Chatbot(Resource):
     def __init__(self):
 
-        # Load data
+        
         loader = WebBaseLoader(URL)
         data = loader.load()
 
-        # Split data into smaller chunks
+        
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         texts = text_splitter.split_documents(data)
 
-        # Create embeddings
+        
         embeddings = OpenAIEmbeddings()
 
-        # Create a vector store (Chroma)
+        
         persist_directory = "chroma_db"
         self.vectordb = Chroma.from_documents(
             documents=texts, embedding=embeddings, persist_directory=persist_directory
         )
 
-        # persist the vector db
+        
         self.vectordb.persist()
 
-        # create retrieval chain
+        
         self.qa = RetrievalQA.from_chain_type(
              llm=GoogleGenerativeAI(model="gemini-pro"), chain_type="stuff", retriever=self.vectordb.as_retriever()
         )
